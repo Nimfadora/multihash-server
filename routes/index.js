6bc088dbf498fb55fb18a1f274b8887f3c5ce695
Nimfadora <leravasilyeva@bk.ru>
2017-11-15 17:56:35 +0300

    add privacy policy

--- routes/index.js
+++ routes/index.js
@@ -7,7 +7,7 @@ router.get('/', function(req, res, next) {
 });
 
 router.get('/privacypolicy', function(req, res, next) {
-  res.render('index', { title: 'Hi, dear!' });
+  res.render('privacypolicy');
 });
 
 module.exports = router;
